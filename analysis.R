411823b5db3267fb22ef6d586e4f67004d0a7526
mcojahme <mcojahme@users.noreply.github.com>
2020-05-12 11:23:38 -0700

    Made Summary Plots

--- analysis.R
+++ analysis.R
@@ -1,3 +1,16 @@
-#Analysis for 2016 City Councel Budget Data
+#Analysis for 2016 City Council Budget Data
 library(dplyr)
 library(ggplot2)
+
+raw.df <- read.csv('data/2016CityCouncilBudgetandExpensesDetailedandSummary_Councillors.csv', skip=3, nrows=10)
+str(raw.df)
+
+df <- raw.df %>%
+  transmute(Councillor = Councillor, Expenses = Total.Councillors.Expenses) %>% 
+  mutate(Expenses = as.numeric(gsub("[$|,]", "", Expenses))) %>% 
+  transform(Councillor = reorder(Councillor, Expenses))
+
+plot <- ggplot(df) + 
+  geom_bar(aes(x=Councillor, y=Expenses), stat="identity") +
+  xlab("") + ylab("Total Expenses (Dollars)") + coord_flip()
+print(plot)
